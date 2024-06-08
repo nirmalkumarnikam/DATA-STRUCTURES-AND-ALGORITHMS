6e98b40d6161a0ba9adced21e04b98d10c22c50e
Nirmalkumar Nikam <nirmalkumarnikam.official@gmail.com>
2024-06-08 15:16:35 +0530

    Added Code of Bubble Sort

--- Sorting/Bubble Sort.cpp
+++ Sorting/Bubble Sort.cpp
@@ -1,7 +1,12 @@
+
+
 Bubble Sort:-
+    at every ith iteration we place ith largest element to its right place
+    
     we will compare betn 1 & 2nd element and swap them if 1>2
     this way we will do n iteration and all the elements will be sorted.
 
+    
     Example: arr = 10   1   7   6   14   9
                     i
     
@@ -108,3 +113,20 @@ Bubble Sort:-
 
                Last element will be already sorted.
                Therefore total round N-1.
+
+
+
+void bubbleSort(int arr[], int n) {
+        
+        //this for loop indicates number of rounds we will perform which is n-1
+        for(int i = 1; i<=n-1; i++){
+            
+            //actual sorting will start here... 
+            //we are iterating till last unsorted element and swapping the unsorted elements.
+            for(int j=0; j<n-i; j++){
+                if(arr[j]>arr[j+1]){
+                    swap(arr[j], arr[j+1]);
+                }
+            }
+        }
+    }
