e83f1075c79b9ff6cda67a7ce0780641701a0dea
Nirmalkumar Nikam <nirmalkumarnikam.official@gmail.com>
2024-06-26 10:08:53 +0530

    New code of MergeSort

--- Sorting/Merge Sort.cpp
+++ Sorting/Merge Sort.cpp
@@ -43,72 +43,72 @@ Algo:
 
 // CODE:
 
-class Solution
-{
-    void merge(int arr[], int l, int m, int r)
-    {
-        int n1 = m-l+1;
-        int n2 = r-m;
-        
-        //creating a temp arrays
-        int L[n1], R[n2];
-        
-        //copy the values in these arrays
-        for(int i = 0; i<n1; i++)
-            L[i]=arr[l+i];
-        
-        for(int j = 0; j<n2; j++)
-            R[j]=arr[m+1+j];
-        
-        
-         /* Merge the temp arrays back into arr[l..r]*/
-        int i = 0, j = 0, k = l;
-        
-        while( i<n1 && j < n2)
+
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void merge(vector<int>&arr, int i, int mid, int j){
+    int arr1Index = i;
+    int arr2Index = mid+1;
+
+    //creating a temp arrays
+    vector<int>merged;
+
+    /* Merge the temp arrays back into arr[l..r]*/
+    while(arr1Index <= mid && arr2Index <= j){
+        if(arr[arr1Index]<arr[arr2Index])
         {
-            if(L[i]<=R[j])
-            {
-                arr[k] = L[i];
-                i++;
-            }
-            else
-            {
-                arr[k] = R[j];
-                j++;
-            }
-            k++;
+            merged.push_back(arr[arr1Index]);
+            arr1Index++;
         }
-        
-        
-        /* Copy the remaining elements*/
-        while(i<n1)
+        else
         {
-            arr[k] = L[i];
-            i++;
-            k++;
-        }
-        
-        while(j<n2)
-        {
-            arr[k]=R[j];
-            j++;
-            k++;
-        }
-        
-    }
-    public:
-    void mergeSort(int arr[], int l, int r)
-    {
-        //code here
-        
-        if(l < r)
-        {
-            int m = l+(r-l)/2;
-            
-            mergeSort(arr, l, m);
-            mergeSort(arr, m+1, r);
-            
-            merge(arr, l, m,r);    
+            merged.push_back(arr[arr2Index]);
+            arr2Index++;
         }
     }
-};
+
+    /* Copy the remaining elements*/
+    while(arr1Index <= mid)
+    {
+        merged.push_back(arr[arr1Index]);
+        arr1Index++;
+    }
+    while(arr2Index <= j)
+    {
+        merged.push_back(arr[arr2Index]);
+        arr2Index++;
+    }
+
+    for(int k=0 ; k<merged.size() ; k++){
+        arr[i]=merged[k];
+        i++;
+    }
+}
+
+void mergesort(vector<int>&arr, int i, int j){
+    
+    if(i>=j)return;
+
+    int mid = (i+j)/2;
+
+    mergesort(arr, i, mid);
+    mergesort(arr,mid+1,j);
+    
+    merge(arr,i, mid, j);
+}
+
+int main(){
+    vector<int>arr{4,3,6,8,3,2,5,3,2,6,29,6,9,0};
+    int n = arr.size();
+
+    mergesort(arr, 0, n-1);
+
+    for(int i = 0; i< n; i++){
+        cout<< arr[i]<<"  ";
+    }
+
+    return 0;
+}
