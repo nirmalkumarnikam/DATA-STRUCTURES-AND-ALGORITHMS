71720cad5c4a64959c23ac13701e6511e552f5b7
Nirmalkumar Nikam <nirmalkumarnikam.official@gmail.com>
2024-06-26 13:36:21 +0530

    just for fun

---
+++ Sorting/quick.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void quicksort(vector<int>&arr){
+    
+
+
+}
+
+int main(){
+    vector<int>arr{4,3,6,8,3,2,5,3,2,6,29,6,9,0};
+    int n = arr.size();
+
+    quicksort(arr);
+
+
+    return 0;
+}
