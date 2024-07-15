c254fd87197a869313861740152068c49e3f3589
Nirmalkumar Nikam <nirmalkumarnikam.official@gmail.com>
2024-07-16 00:20:06 +0530

    binary tree intro done

--- Binary Tree/Introduction.cpp
+++ Binary Tree/Introduction.cpp
@@ -63,8 +63,45 @@ void LevelOrderTraversal(node*root){
             }
         }
     }
-
 }
+
+void buildFromLevelOrderTraversal(node* &root){
+    int data;
+    cout<<"Enter Root Data: ";
+    cin>>data;
+
+    root = new node(data);
+
+    queue<node*> q;
+
+    q.push(root);
+
+    while(!q.empty()){
+        node* temp = q.front();
+        q.pop();
+
+        cout<<"Enter the Data to the left of "<<temp->data<<" ";
+        int leftData;
+        cin>>leftData;
+        if(leftData!=-1){
+            node* leftNode = new node(leftData);
+            temp->left = leftNode;
+            q.push(leftNode);
+        }
+
+
+
+        cout<<"Enter the Data to the right of "<<temp->data<<" ";
+        int rightData;
+        cin>>rightData;
+        if(rightData!=-1){
+            node* rightNode = new node(rightData);
+            temp->right = rightNode;
+            q.push(rightNode);
+        }
+    }
+}
+
 
 void ReverseLevelOrderTraversal(node*root){
     queue<node*>q;
@@ -106,15 +143,58 @@ void ReverseLevelOrderTraversal(node*root){
     }
 }
 
+
+void inorder(node* root){
+    if(root == NULL){
+        return;
+    }
+
+    inorder(root->left);
+    cout<<" "<<root->data<<" ";
+    inorder(root->right);
+}
+
+void preorder(node* root){
+    if(root == NULL){
+        return;
+    }
+
+    cout<<" "<<root->data<<" ";
+    preorder(root->left);
+    preorder(root->right);
+}
+
+void postorder(node* root){
+    if(root == NULL){
+        return;
+    }
+
+    postorder(root->left);
+    postorder(root->right);
+    cout<<" "<<root->data<<" ";
+}
+
+
+
 int main(){
     cout<<"hello tree"<<endl;
     node* root = NULL;
     // Binart Tree: 1 3 5 7 11 17
     //Enter data: 1 3 7 -1 -1 11 -1 -1 5 17 -1 -1 -1
-    root = buildTree(root);
+    // root = buildTree(root);
+    buildFromLevelOrderTraversal(root);
     cout<<"\nLevelOrderTraversal: "<<endl;
     LevelOrderTraversal(root);
     cout<<"\nReverceLevelOrderTraversal: "<<endl;
     ReverseLevelOrderTraversal(root);
+
+    cout<<"\nInorder Traversal:";
+    inorder(root);
+    cout<<"\nPreOrder Traversal:";
+    preorder(root);
+    cout<<"\nPostorder Traversal:";
+    postorder(root);
+
+
     return 0;
 }
