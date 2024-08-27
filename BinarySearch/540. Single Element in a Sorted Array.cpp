152b4901cd87f8cfb6686266d19ef135fb805f8c
Nirmalkumar <105733313+nirmalkumarnikam@users.noreply.github.com>
2024-08-27 19:24:25 +0530

    Update 540. Single Element in a Sorted Array.cpp

--- BinarySearch/540. Single Element in a Sorted Array.cpp
+++ BinarySearch/540. Single Element in a Sorted Array.cpp
@@ -1,24 +1,24 @@
-// class Solution {
-// public:
-//     int singleNonDuplicate(vector<int>& nums) {
-//         int n = nums.size();
-//         if(n == 1)return nums[0];
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+        int n = nums.size();
+        if(n == 1)return nums[0];
 
-//         // here we are cheking couple, if found we move 2 places ahead
+        // here we are cheking couple, if found we move 2 places ahead
 
-//         for(int i =0; i<n; i++)
-//         {
-//             if(nums[i]==nums[i+1]){
-//                 i++;
-//             }else
-//             {
-//                 return nums[i];
-//             }
-//         }
+        for(int i =0; i<n; i++)
+        {
+            if(nums[i]==nums[i+1]){
+                i++;
+            }else
+            {
+                return nums[i];
+            }
+        }
 
-//         return -1;
-//     }
-// };
+        return -1;
+    }
+};
 
 
 
