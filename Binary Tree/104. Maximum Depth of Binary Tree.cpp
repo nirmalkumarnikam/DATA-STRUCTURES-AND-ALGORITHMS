50bc884e6d0ad2a0372e3e1ecd307c74d56ac58a
Nirmalkumar <105733313+nirmalkumarnikam@users.noreply.github.com>
2024-09-05 23:57:45 +0530

    Update 104. Maximum Depth of Binary Tree.cpp

--- Binary Tree/104. Maximum Depth of Binary Tree.cpp
+++ Binary Tree/104. Maximum Depth of Binary Tree.cpp
@@ -52,3 +52,16 @@ public:
         return count;
     }
 };
+
+
+
+
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+       if(root == NULL){
+            return 0;
+       }
+       return (1+max(maxDepth(root->left), maxDepth(root->right))); 
+    }
+};
